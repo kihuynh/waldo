db786d8238648905cd2215a3a5df472381f847fa
Frank Ngo and Kimberly Huynh <kimberly@>
2018-01-09 11:17:22 -0800

    add button and bg color

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
     $(".not-showing-2").fadeToggle();
-    // $(".not-showing").fadeToggle();
-
     });
   $(".clickable-2").click(function() {
     $(".showing-2").fadeToggle();
-    // $(".not-showing").fadeToggle();
-    // $(".showing").toggle();
     });
   $(".clickable-3").click(function() {
-
     $(".showing").fadeToggle();
+  });
 
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
 
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
   });
 });
